0f0411fe852279345efcccf31bf92683fd7b28ed
Sergey Anufrienko <serg@anufrienko.net>
2016-03-02 12:07:05 +0300

    dynamically instantiate server from config

--- scxmppd/src/main/scala/com/scxmpp/server/Server.scala
+++ scxmppd/src/main/scala/com/scxmpp/server/Server.scala
@@ -7,21 +7,26 @@ import io.netty.buffer.PooledByteBufAllocator
 import io.netty.channel.epoll.{Epoll, EpollEventLoopGroup, EpollServerSocketChannel}
 import io.netty.channel.nio.NioEventLoopGroup
 import io.netty.channel.socket.nio.NioServerSocketChannel
-import io.netty.channel.{Channel, ChannelOption, EventLoopGroup, ServerChannel}
+import io.netty.channel.{Channel, ChannelOption, EventLoopGroup, ServerChannel, ChannelInitializer}
 
 import scala.collection.JavaConversions._
 import scala.collection.mutable.ArrayBuffer
+import com.typesafe.config.Config
 
 class Server(context: ServerContext) {
 
   var channels = ArrayBuffer.empty[Channel]
-
-  val bootstrap = new ServerBootstrap()
-  bootstrap.childOption(ChannelOption.ALLOCATOR, new PooledByteBufAllocator(true))
+  var bootstraps = ArrayBuffer.empty[ServerBootstrap]
 
   private def doRun(group: EventLoopGroup, clazz: Class[_ <: ServerChannel]): Unit = {
     try {
       for(server <- context.servers) {
+        val bootstrap = new ServerBootstrap()
+        bootstrap.childOption(ChannelOption.ALLOCATOR, new PooledByteBufAllocator(true))
+        val initializer = context.dynamicAccess.createInstanceFor[ChannelInitializer[Channel]](
+          server.getString("module"), List(classOf[ServerContext] -> context, classOf[Config] -> server)).get
+        bootstrap.group(group).channel(clazz).childHandler(initializer)
+
         val inet: InetSocketAddress = new InetSocketAddress(
           server.getString("endpoint.address"), server.getInt("endpoint.port"))
         channels.add(bootstrap.bind(inet).sync.channel)
